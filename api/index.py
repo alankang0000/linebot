f2d97b4dd27059a2ebf5b5105c8d24c92c45f9cd
alanyiwei <123930551+alankang0000@users.noreply.github.com>
2023-02-13 16:01:20 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,17 +2,21 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os, openai
+import os
+import openai
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-openai.api_key =os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
 # domain root
+
+
 @app.route('/')
 def home():
     return 'Hello, World!'
+
 
 @app.route("/webhook", methods=['POST'])
 def callback():
@@ -31,28 +35,28 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    if (event.message.text[:5] == "@小小秘豬"):
-        response_text = event.message.text[5:]
+    if (event.message.text[:4] == "@小秘豬"):
+        response_text = event.message.text[4:]
         response = openai.Completion.create(
             model="code-davinci-002",
-            prompt= response_text,
-            temperature=0.1,
-            max_tokens=200,
+            prompt="\"\"\"\n"+response_text+"\n\"\"\" ",
+            temperature=0.0,
+            max_tokens=250,
             top_p=1,
-            frequency_penalty=0.5,
+            frequency_penalty=0.0,
             presence_penalty=0,
-            )
+            stop=["\"\"\""]
+        )
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=response["choices"][0]["text"].replace('\n'))
-            )
+            TextSendMessage(text=response["choices"][0]["text"])
+        )
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="I couldn’t catch what you said.")
-            )
+        )
     return
-
 
 
 if __name__ == "__main__":
