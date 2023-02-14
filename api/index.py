4861c6f5800743bd3834939e48f60f0b3ea4cd30
alanyiwei <123930551+alankang0000@users.noreply.github.com>
2023-02-14 11:15:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -43,8 +43,8 @@ def echo(event):
             temperature=0.05,
             max_tokens=200,
             top_p=1,
-            frequency_penalty=0.0,
-            presence_penalty=0.2,
+            frequency_penalty=0.2,
+            presence_penalty=0.1,
         )
         line_bot_api.reply_message(
             event.reply_token,
